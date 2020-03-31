efb5ae922247c844e1e0207e58eb9cfd4b8733d7
Richard Guy <rich.guy@cantab.net>
2020-03-31 20:48:58 +0100

    Add countdown progress bar

--- app/static/timer.js
+++ app/static/timer.js
@@ -1,3 +1,11 @@
-function autoRefresh(t) {
+function startTimer(t) {
+  var progressBarAnimation = document.getElementById('countdown').animate(
+    [
+      { width: 100 * (t / 30000) + '%' },
+      { width: '0%' }
+    ],
+    {
+      duration: t
+    });
   setTimeout("location.reload(true);", t);
 }
--- run.py
+++ run.py
@@ -2,3 +2,9 @@
 from app import create_app, db
 
 app = create_app()
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=80
+    )
